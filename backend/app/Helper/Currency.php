f8266d309a091945111b7efa8f8c55f042999aa6
Manuel Maier <manuel@morethings.digital>
2024-11-20 23:16:19 +0100

    fix: rounding precision

--- backend/app/Helper/Currency.php
+++ backend/app/Helper/Currency.php
@@ -13,20 +13,12 @@ class Currency
         return $formatter->formatCurrency($amount, $currencyCode);
     }
 
-    public static function round(float $value, $precision = 2): float
+    public static function round(float $value, $precision = 1): float
     {
-        $fraction = $value - floor($value);
-
-        if ($fraction >= 0.99) {
-            return ceil($value);
-        } elseif ($fraction <= 0.01) {
-            return floor($value);
-        } else {
-            return round(
-                num: $value,
-                precision: $precision,
-                mode: PHP_ROUND_HALF_UP
-            );
-        }
+        return round(
+            num: $value,
+            precision: $precision,
+            mode: PHP_ROUND_HALF_UP
+        );
     }
 }
