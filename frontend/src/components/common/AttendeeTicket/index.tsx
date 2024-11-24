0dbf96654d2d9956fc40f9b5f09953b70ee7dbfc
Manuel Maier <manuel@morethings.digital>
2024-11-24 22:26:45 +0100

    feat: another one

--- frontend/src/components/common/AttendeeTicket/index.tsx
+++ frontend/src/components/common/AttendeeTicket/index.tsx
@@ -92,13 +92,22 @@ export const AttendeeTicket = ({attendee, ticket, event, hideButtons = false}: A
                 </div>
             </Card>
             <Card className={classes.attendee}>
-                <h2>Mit freundlicher Unterstützung von:</h2>
-                <div className={classes.sponsorImagesWrapper}>
-                    <img className={classes.sponsorImage} src={"https://placehold.co/600x400/000000/FFF"}/>
-                    <img className={classes.sponsorImage} src={"https://placehold.co/600x400/000000/FFF"}/>
-                    <img className={classes.sponsorImage} src={"https://placehold.co/600x400/000000/FFF"}/>
-                    <img className={classes.sponsorImage} src={"https://placehold.co/600x400/000000/FFF"}/>
-                    <img className={classes.sponsorImage} src={"https://placehold.co/600x400/000000/FFF"}/>
+                <div>
+                    <h2>Mit freundlicher Unterstützung von:</h2>
+                    <div className={classes.sponsorImagesWrapper}>
+                        <div className={classes.sponsorImageElement}>
+                            <img className={classes.sponsorImage} src={"https://placehold.co/600x400/000000/FFF"}/>
+                        </div>
+                        <div className={classes.sponsorImageElement}>
+                            <img className={classes.sponsorImage} src={"https://placehold.co/600x400/000000/FFF"}/>
+                        </div>
+                        <div className={classes.sponsorImageElement}>
+                            <img className={classes.sponsorImage} src={"https://placehold.co/600x400/000000/FFF"}/>
+                        </div>
+                        <div className={classes.sponsorImageElement}>
+                            <img className={classes.sponsorImage} src={"https://placehold.co/600x400/000000/FFF"}/>
+                        </div>
+                    </div>
                 </div>
             </Card>
         </>
